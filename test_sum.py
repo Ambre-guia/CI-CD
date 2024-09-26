638c45e9f6e7588eea41f9c27c9cc33fd19b56b7
Ambre-guia <ambreguia77@gmail.com>
2024-09-26 10:50:43 +0200

    change test_sum

--- test_sum.py
+++ test_sum.py
@@ -1,11 +1,12 @@
-import unittest
 from sum import add
 
-class TestSum(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(add(2, 3), 5)
-        self.assertEqual(add(-1, 1), 0)
-        self.assertEqual(add(0, 0), 0)
+def test_add():
+    """Tests for the add function."""
+    assert add(2, 3) == 5, "Test failed: 2 + 3 should be 5"
+    assert add(-1, 1) == 0, "Test failed: -1 + 1 should be 0"
+    assert add(0, 0) == 0, "Test failed: 0 + 0 should be 0"
+    
+    print("All tests passed!")
 
 if __name__ == '__main__':
-    unittest.main()
+    test_add()
